ca9e00d51f0402d052bb465117ab7660ad429c89
okkyarrohman <okkyzee@gmail.com>
2024-02-05 11:28:43 +0700

    session login and total login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,4 +34,4 @@ Route::group(['middleware' => 'role:murid'], function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
