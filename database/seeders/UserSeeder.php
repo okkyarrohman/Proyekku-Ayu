6282afb56f43d3b17f0d73854dfe8dda4e660706
okkyarrohman <okkyzee@gmail.com>
2024-02-05 11:38:02 +0700

    role user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $guru = User::create([
+            'name' => 'guru Proyekku',
+            'email' => 'guru@proyekku.com',
+            'password' => bcrypt('guru123')
+        ]);
+        $guru->assignRole('guru');
+
+        $murid = User::create([
+            'name' => 'murid Proyekku',
+            'email' => 'murid@proyekku.com',
+            'password' => bcrypt('murid123')
+        ]);
+        $murid->assignRole('murid');
+    }
+}
