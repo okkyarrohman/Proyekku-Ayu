debd97e6ab1b9278c10f7863bf741234dcca53b6
Febrian Daffa Eka Putra <febriandaffa123@gmail.com>
2024-04-26 10:23:45 +0700

    feat: backend dashboard

--- resources/js/Pages/Guru/Dashboard.jsx
+++ resources/js/Pages/Guru/Dashboard.jsx
@@ -1,6 +1,11 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { usePage } from "@inertiajs/react";
 
 export default function Dashboard({ auth }) {
+    const { absensis } = usePage().props;
+
+    console.log(absensis);
+
     return (
         <AuthenticatedLayout
             authUser={auth.user}
