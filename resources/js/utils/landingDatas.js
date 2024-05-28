b0d031526ce201ec6739870d477c2986ab2be080
okkyarrohman <okkyzee@gmail.com>
2024-05-28 07:36:52 +0700

    landingPages

--- resources/js/utils/landingDatas.js
+++ resources/js/utils/landingDatas.js
@@ -2,29 +2,29 @@ export const featureItems = [
     {
         img: "/assets/feature1-image.jpeg",
         title: "Ruang Proyek",
-        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam harum necessitatibus consectetur, ad minima animi perspiciatis doloribus",
+        desc: "Area kolaboratif di dalam platform ProyekKu Learning Management System yang memungkinkan siswa dan pengajar bekerja bersama dalam mengeksekusi proyek-proyek pembelajaran.",
     },
     {
         img: "/assets/feature2-image.jpeg",
         title: "Konten Belajar",
-        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam harum necessitatibus consectetur, ad minima animi perspiciatis doloribus",
+        desc: " Konten belajar disusun secara sistematis untuk mendukung proses pembelajaran yang terstruktur dan efektif.",
     },
     {
         img: "/assets/feature3-image.jpeg",
         title: "Laporan Hasil Belajar",
-        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam harum necessitatibus consectetur, ad minima animi perspiciatis doloribus",
+        desc: "Memberikan gambaran holistik tentang prestasi siswa dan dapat digunakan sebagai dasar untuk memberikan umpan balik dan pengambilan keputusan terkait proses pembelajaran",
     },
 ];
 
 export const aboutItems = [
     {
-        desc: "Lorem ipsum dolor sit amet consectetur. Elementum nisl duis tortor sed.",
+        desc: "Kolaboratif dan Interaktif: ProyekKu memfasilitasi kolaborasi antara siswa dan pengajar secara online, memungkinkan pertukaran ide, diskusi, dan kerja tim yang efektif.",
     },
     {
-        desc: "Lorem ipsum dolor sit amet consectetur. Elementum nisl duis tortor sed.",
+        desc: "Manajemen Pembelajaran yang Efisien: Dengan fitur-fitur seperti Ruang Proyek, Konten Belajar terstruktur, dan Laporan Hasil Belajar, ProyekKu membantu dalam mengelola, melacak, dan mengevaluasi kemajuan belajar siswa secara efisien.",
     },
     {
-        desc: "Lorem ipsum dolor sit amet consectetur. Elementum nisl duis tortor sed.",
+        desc: "Teknologi Canggih dan Pendekatan Inovatif: Melalui teknologi mutakhir, ProyekKu memberikan pengalaman belajar yang menarik dan memanfaatkan pendekatan inovatif untuk meningkatkan hasil pembelajaran.",
     },
     {
         desc: "Lorem ipsum dolor sit amet consectetur. Elementum nisl duis tortor sed.",
